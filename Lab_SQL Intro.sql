5160b08fb053e1c0aa237b66aa61f930843d8bd9
Wailbak <wailbak11@gmail.com>
2024-03-15 19:55:29 +0100

    Activity Finished

---
+++ Lab_SQL Intro.sql
@@ -0,0 +1,16 @@
+select * from actor;
+select * from film;
+select * from customer;
+
+SELECT film.title FROM film;
+
+SELECT DISTINCT language_id AS language FROM language;
+
+SELECT COUNT(*) AS store_count
+FROM store;
+
+SELECT COUNT(*) AS employee_count
+FROM staff;
+
+SELECT first_name AS employee_first_name
+FROM staff;
